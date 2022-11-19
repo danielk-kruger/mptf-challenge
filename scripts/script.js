ea78bea0572fa84ad0a4769c13a12e83543a721e
Danielk <74910071+danielk-kruger@users.noreply.github.com>
2022-11-19 17:05:15 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,37 @@
-var typed = new Typed('#typed', {
-    strings: ["^2000 Seja bem vindo!", "O tenis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."],
-    typeSpeed: 40
-  });
+window.addEventListener("load", () => {
+  document.querySelectorAll(".image").forEach((img) => {
+    const preloadElem = document.createElement("link");
+    preLoadImage.href = `${img.src}`;
+    preloadElem.rel = "preload";
+    preloadElem.as = "image";
+    preloadElem.crossOrigin = "anonymous";
 
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '50px',
-    duration: 2500,
-    reset: true
+    document.head.appendChild(preloadElem);
+  });
 });
 
-ScrollReveal().reveal('header p', { delay: 100 });
-ScrollReveal().reveal('header h3', { delay: 200 });
+var typed = new Typed("#typed", {
+  strings: [
+    "^2000 Seja bem vindo!",
+    "O tenis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan.",
+  ],
+  typeSpeed: 40,
+});
 
-ScrollReveal().reveal('#home .image', { delay: 100 });
-ScrollReveal().reveal('#home .text', { delay: 300 });
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 2500,
+  reset: true,
+});
 
-ScrollReveal().reveal('#spotlight .text', { delay: 400 });
+ScrollReveal().reveal("header p", { delay: 100 });
+ScrollReveal().reveal("header h3", { delay: 200 });
 
-ScrollReveal().reveal('.shoe', { delay: 500 });
-ScrollReveal().reveal('footer', { delay: 600 });
+ScrollReveal().reveal("#home .image", { delay: 100 });
+ScrollReveal().reveal("#home .text", { delay: 300 });
+
+ScrollReveal().reveal("#spotlight .text", { delay: 400 });
+
+ScrollReveal().reveal(".shoe", { delay: 500 });
+ScrollReveal().reveal("footer", { delay: 600 });
